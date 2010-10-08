42ebacc4fde3eac6a82e02d7e68d8a12dafe3880
Dan Davison <davison@stats.ox.ac.uk>
2010-10-08 15:43:15 +0100

    refactor

--- lib/libchromopainter.R
+++ lib/libchromopainter.R
@@ -1,17 +1,15 @@
-chromopainter.read.donor.copy.counts <- function(dir) {
-    files <- list.files(path=file.path(dir, "LeaveOneOutNeResults"),
-                        pattern=sprintf("...Chrom%sLeaveOneOutNeEst.chunkcounts.out", chrom),
-                        full.names=TRUE)
+chromopainter.read.counts <- function(files) {
     names(files) <- substr(basename(files), 1, 3)
     x <- sapply(files, read.table, row.names=1, header=TRUE, simplify=FALSE)
     lapply(x, as.matrix)
 }
 
-chromopainter.read.recipient.copy.counts <- function(dir) {
-    files <- list.files(path=file.path(dir, "PaintingSamples"),
-                        pattern=sprintf("...Chrom%sPaintingNeEstSamples.chunklengths.out", chrom),
-                        full.names=TRUE)
-    names(files) <- substr(basename(files), 1, 3)
-    x <- sapply(files, read.table, row.names=1, header=TRUE, simplify=FALSE)
-    lapply(x, as.matrix)
-}
+chromopainter.read.donor.copy.counts <- function(dir)
+    chromopainter.read.counts(list.files(path=file.path(dir, "LeaveOneOutNeResults"),
+                                         pattern=sprintf("...Chrom%sLeaveOneOutNeEst.chunkcounts.out", chrom),
+                                         full.names=TRUE))
+
+chromopainter.read.recipient.copy.counts <- function(dir)
+    chromopainter.read.counts(list.files(path=file.path(dir, "PaintingSamples"),
+                                         pattern=sprintf("...Chrom%sPaintingNeEstSamples.chunkcounts.out", chrom),
+                                         full.names=TRUE))
