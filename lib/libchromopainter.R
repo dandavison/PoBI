d8c85bd2c520ec0dbb66f06a4defa8611fd27e0f
Dan Davison <davison@stats.ox.ac.uk>
2010-10-12 15:18:49 +0100

    chromopainter: error check in read* code

--- lib/libchromopainter.R
+++ lib/libchromopainter.R
@@ -1,6 +1,8 @@
 chromopainter.read.chunk.table <- function(files) {
     names(files) <- substr(basename(files), 1, 3)
     x <- sapply(files, read.table, row.names=1, header=TRUE, simplify=FALSE)
+    z <- sapply(x, colnames)
+    stopifnot(z == z[,1])
     lapply(x, as.matrix)
 }
 
