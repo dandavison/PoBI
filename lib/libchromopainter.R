4c64c1a2addf6a3ef70b20c27de3a300f6b79943
Dan Davison <davison@stats.ox.ac.uk>
2010-10-12 15:18:09 +0100

    chromopainter.read.*.chunk.*: definition was missing formal argument

--- lib/libchromopainter.R
+++ lib/libchromopainter.R
@@ -4,17 +4,17 @@ chromopainter.read.chunk.table <- function(files) {
     lapply(x, as.matrix)
 }
 
-chromopainter.read.donor.chunk.counts <- function(dir)
+chromopainter.read.donor.chunk.counts <- function(dir, chrom)
     chromopainter.read.chunk.table(list.files(path=file.path(dir, "LeaveOneOutNeResults"),
                                               pattern=sprintf("...Chrom%sLeaveOneOutNeEst\\.chunkcounts\\.out", chrom),
                                               full.names=TRUE))
 
-chromopainter.read.recipient.chunk.counts <- function(dir)
+chromopainter.read.recipient.chunk.counts <- function(dir, chrom)
     chromopainter.read.chunk.table(list.files(path=file.path(dir, "PaintingSamples"),
                                               pattern=sprintf("...Chrom%sPaintingNeEstSamples\\.chunkcounts\\.out", chrom),
                                               full.names=TRUE))
 
-chromopainter.read.recipient.chunk.lengths <- function(dir)
+chromopainter.read.recipient.chunk.lengths <- function(dir, chrom)
     chromopainter.read.chunk.table(list.files(path=file.path(dir, "PaintingSamples"),
                                               pattern=sprintf("...Chrom%sPaintingNeEstSamples\\.chunklengths\\.out", chrom),
                                               full.names=TRUE))
