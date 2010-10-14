d13887fb2cf47de7782c3f54153f9e91117efc90
Dan Davison <davison@stats.ox.ac.uk>
2010-10-14 15:19:24 +0100

    Bugfixes

--- lib/libchromopainter.R
+++ lib/libchromopainter.R
@@ -1,22 +1,22 @@
-cp.read.chunk.table <- function(chrom, dir, subdir, id, what, exclude)
-    cp.read.chunk.table(cp.list.files(chrom, file.path(dir, subdir), id=id, what=what), exclude)
-
 cp.read.donor.chunk.counts <- function(chrom, dir, exclude)
     cp.read.chunk.table(chrom, dir, "LeaveOneOutNeResults", "LeaveOneOutNeEst", "chunkcounts.out", exclude)
 
 cp.read.recipient.chunk.counts <- function(chrom, dir, exclude)
-    cp.read.chunk.table(chrom, dir, "PaintingNeResults", "LeaveOneOutNeEst", "chunkcounts.out", exclude)
+    cp.read.chunk.table(chrom, dir, "PaintingNeResults", "PaintingNeEst", "chunkcounts.out", exclude)
 
 cp.read.recipient.chunk.lengths <- function(chrom, dir, exclude)
-    cp.read.chunk.table(chrom, dir, "PaintingNeResults", "LeaveOneOutNeEst", "chunklengths.out", exclude)
+    cp.read.chunk.table(chrom, dir, "PaintingNeResults", "PaintingNeEst", "chunklengths.out", exclude)
 
-cp.read.chunk.table <- function(files, exclude) {
+cp.read.chunk.table <- function(chrom, dir, subdir, id, what, exclude)
+    cp.read.chunk.table.internal(cp.list.files(chrom, file.path(dir, subdir), id=id, what=what), exclude)
+
+cp.read.chunk.table.internal <- function(files, exclude) {
     names(files) <- substr(basename(files), 1, 3)
     if(!missing(exclude)) files <- files[! names(files) %in% exclude]
 
     x <- sapply(files, read.table, row.names=1, header=TRUE, simplify=FALSE)
     z <- sapply(x, colnames)
-    stopifnot(z == z[,1])
+    stopifnot(length(x) == 0 || z == z[,1])
     lapply(x, as.matrix)
 }
 
